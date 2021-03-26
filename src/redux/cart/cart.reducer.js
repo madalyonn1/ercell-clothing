535b7ad9c76d39d18cad9287b548832eeeaf4e81
madalyonn1 <arslanersel96@gmail.com>
2021-03-26 15:57:19 +0300

    Fixed cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -27,7 +27,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter(
-          (cartItem) => cartItem.id !== action.payload
+          (cartItem) => cartItem.id !== action.payload.id
         ),
       };
     default:
