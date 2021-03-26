16c09788a8c0135da983771a9faba839cc716fec
madalyonn1 <arslanersel96@gmail.com>
2021-03-27 02:20:53 +0300

    Updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IZNrkIMcLs98PLg071Op5DKFaJ4taT8SYP5sFw6DAlIXoORY2cRc0O68NU1dqx6nGInI4m3kyHbBlSFKcm9b16r00FpIqVjHW";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Ercell Clothing Ltd."
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
